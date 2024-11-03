f859284faeb8d67dd6a517c0d24207877f1dafde
manyuagashe <manyu@unc.edu>
2024-11-03 10:55:34 -0500

    sidebar changes (not working rn)

--- kondomeeneum/src/app/page.js
+++ kondomeeneum/src/app/page.js
@@ -1,10 +1,14 @@
 "use client";
 import DraggableSquares from "@/components/DraggableSquares";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
-    <>
-      <DraggableSquares />
-    </>
-  )
+    <div className="flex h-screen w-full overflow-hidden">
+      <Sidebar />
+      <div className="flex-1 relative">
+        <DraggableSquares />
+      </div>
+    </div>
+  );
 }
